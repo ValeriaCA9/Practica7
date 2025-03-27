2498af48e4bcae9953c9a4c7e78c492653c73c5f
ValeriaCA9 <vale38646@gmail.com>
2025-03-27 14:06:09 -0600

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,1 +1,20 @@
+//Cruz Alegria Valeria Montserrat
 
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    
+    printf ("Dame un numero: ");
+    scanf("%i", &a);
+    
+    if (a%2==0)
+    {
+        printf("par\n");
+    }
+    else
+    {
+    printf("impar\n");
+    }
+}
