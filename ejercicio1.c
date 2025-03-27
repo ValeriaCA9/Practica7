08b9436eb5bf304b75a944e204bebf87097357d7
ValeriaCA9 <vale38646@gmail.com>
2025-03-27 13:57:17 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,18 @@
+//Cruz Alegria Valeria Montserrat 
+#include <stdio.h>
 
+void main()
+{
+    int a, b;
+    
+    printf ("Dame un numero: ");
+    scanf("%i", &a);
+    printf ("Dame un numero: ");
+    scanf("%i", &b);
+    
+    if (a >b)
+    {
+        printf("a>b ?\n");
+    }
+    printf("a<b ?\n");
+}
